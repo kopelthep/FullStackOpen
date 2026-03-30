a0a4504d4319d515ffe567aa6a68a843b1fd4902
kopelthep <47904193+kopelthep@users.noreply.github.com>
2026-03-30 16:28:14 -0400

    1.4 Turns out me placing stuff in arrays is what was wanted but AFTER. refactor to follow the method of placing objects into array

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -9,7 +9,7 @@ const Header = (props) => {
   )
 }
 const Content = (props) => {
-  console.log(props)
+  //console.log(props)
   return (
     <div>
       <Part part = {props.parts[0]} />
@@ -19,11 +19,11 @@ const Content = (props) => {
   )
 }
 const Total = (props) => {
-  console.log("total",props)
-  console.log(props.exercises[0]+props.exercises[1]+props.exercises[2])
+  //console.log("total",props)
+  
   return (
     <p>
-      Number of exercises {(props.exercises[0]+props.exercises[1]+props.exercises[2])}
+      Number of exercises {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises}
     </p>
   )
 }
@@ -41,33 +41,32 @@ const Part = (props) => {
 const App = () => {
 
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
 
-  //const course = 'Half Stack application development'
-  //const part1 = 'Fundamentals of React'
-  //const exercises1 = 10
-  //const part2 = 'Using props to pass data'
-  //const exercises2 = 7
-  //const part3 = 'State of a component'
-  //const exercises3 = 14
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
+ 
+
+  //console.log("Master log",parts)
+  
   return (
     <div>
       <Header course = {course} />
-      <Content parts = {[part1,part2,part3]}  />
+      <Content parts = {parts}  />
       
-      <Total exercises = {[part1.exercises, part2.exercises, part3.exercises]}/>
+      <Total parts = {parts}/>
       
     </div>
   )
