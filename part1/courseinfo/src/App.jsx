aa038abbfbc2dc1cc20a72a04411c7017139aecb
kopelthep <47904193+kopelthep@users.noreply.github.com>
2026-03-30 13:46:35 -0400

    Exercises 1.1 and 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -9,11 +9,11 @@ const Header = (props) => {
 }
 const Content = (props) => {
   return (
-    <>
+    <div>
       <Part part = {props.parts[0]} exercise = {props.exercises[0]}/>
       <Part part = {props.parts[1]} exercise = {props.exercises[1]}/>
       <Part part = {props.parts[2]} exercise = {props.exercises[2]}/>
-    </>
+    </div>
   )
 }
 const Total = (props) => {
