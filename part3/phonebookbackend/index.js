a4f367903f1ba09e0351f700bc25c59006efac21
kopelthep <47904193+kopelthep@users.noreply.github.com>
2026-04-20 16:08:46 -0400

    3.2 and  3.3

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+
+
+
+
 let persons = [
     { 
         "id": "1",
@@ -24,12 +28,40 @@ let persons = [
         "number": "39-23-6423122"
     }
 ]
+
+const currentStatus = () => {
+    const length = persons.length
+    const datenow = Date.now()
+    const realtimestamp = Date(datenow).toString()
+    console.log(realtimestamp)
+    const responseString = String ("<p> Phonebook has info on " + String(length) + " people </p>") + ("<p>"+ realtimestamp +"</p>")
+    return(
+        responseString
+    )
+
+}
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
+app.get("/info",(request,response)=>{
+    response.send(currentStatus())
+})
+
 app.get('/api/persons', (request, response) => {
     response.json(persons) 
+})
+
+app.get("/api/persons/:id",(request,response)=> {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } 
+    else {
+        response.status(404).end()
+    }
 })
 
 const PORT = 3001
