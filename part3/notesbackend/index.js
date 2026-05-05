a286cb3b78a7f296fd0c58bed3833bcfedf6556e
kopelthep <47904193+kopelthep@users.noreply.github.com>
2026-05-05 14:39:32 -0400

    Prep for last exo of Part 3

--- part3/notesbackend/index.js
+++ part3/notesbackend/index.js
@@ -65,7 +65,7 @@ app.post('/api/notes', (request, response,next) => {
   note.save().then((savedNote) => {
     response.json(savedNote)
   })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -89,7 +89,7 @@ app.put('/api/notes/:id', (request, response, next) => {
 
 app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
-    .then((result) => {
+    .then(() => {
       response.status(204).end()
     })
     .catch((error) => next(error))
